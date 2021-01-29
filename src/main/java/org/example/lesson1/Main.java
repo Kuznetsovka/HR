f87ef16b12ede749c419321bfd0ffa0145d84db8
Kirill Kuznetsov <kuznechik.86@mail.ru>
2021-01-29 22:00:23 +0300

    Init commit

---
+++ src/main/java/org/example/lesson1/Main.java
@@ -0,0 +1,9 @@
+package org.example.lesson1;
+
+public class Main {
+    /*
+    1. Создать builder для класса Person со следующими полями: String firstName, String lastName, String middleName, String country, String address, String phone, int age, String gender.
+    2. Описать ошибки в коде (см. задание в методичке) и предложить варианты оптимизации.
+    3. Написать пример кода, который реализует принцип полиморфизма, на примере фигур — круг, квадрат, треугольник.
+     */
+}
